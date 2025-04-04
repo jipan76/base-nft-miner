3264714fa03b7d3984074157c7e83ab9665d0932
DitYa_eth <35645327+jipan76@users.noreply.github.com>
2025-04-04 07:23:31 +0700

    Update NftMinter.sol

--- contracts/NftMinter.sol
+++ contracts/NftMinter.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 contract NFTMinter is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Shardeum Dev NFTMinter", "SNFT") {}
+    constructor() ERC721("EsTea", "TEA") {}
 
     function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
